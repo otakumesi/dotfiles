1efa26ff4ed4b1ccf0a1e33ff3bd418f5f9905c7
Takuro Niitsuma <takuro.niitsuma@livesense.co.jp>
2017-12-28 09:17:06 +0900

    ansible first commit

--- init.d/init.os.d/init.mac.sh
+++ init.d/init.os.d/init.mac.sh
@@ -1,5 +1,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew intall ansible
+brew install git
 brew install rbenv
 brew install tfenv
 brew cask install amethyst
