a21593d5f1a6942577480521dd641f73370892d9
otakumesi <bakednt@gmail.com>
2019-11-17 12:44:28 +0900

    add clipboard-func

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,19 @@
 (column-number-mode t)
 (size-indication-mode t)
 
+(setq x-select-enable-clipboard t)
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
